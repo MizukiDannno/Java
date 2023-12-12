6814cd56e744bf9101da745d96ef0abbf2afb741
MizukiDanno <kumanoya9310@gmail.com>
2023-12-12 19:50:49 +0900

    課題1.23です。ご確認お願い致します。

---
+++ HelloWorld_DannoMizuki/Java1.23/executeClass/Animal.java
@@ -0,0 +1,29 @@
+package executeClass;
+
+import processClass.Lion;
+
+/*
+
+	下記がコンソールに出力されるように作成してください
+	※thisとsetterとgetterとフィールドを使ってください
+
+	動物名：ライオン
+体長：2.1m
+速度：80km/h
+
+ */
+
+public class Animal {
+
+	public static void main(String[] args) {
+		
+        // Lionオブジェクトを作成
+        Lion lion = new Lion("ライオン", 2.1, 80);
+
+        // コンソールに出力
+        System.out.println("動物名：" + lion.getAnimalName());
+        System.out.println("体長：" + lion.getAnimalSize() + "m");
+        System.out.println("速度：" + lion.getAnimalSpeed() + "km/h");
+        
+	}
+}
---
+++ HelloWorld_DannoMizuki/Java1.23/processClass/Lion.java
@@ -0,0 +1,58 @@
+package processClass;
+
+public class Lion {
+
+	// フィールド変数
+	public String animalName;
+	public double animalSize;
+	public int animalSpeed;
+
+
+	// コンストラクタを作成
+	public Lion(String animalName, double animalSize, int animalSpeed) {
+
+		// Nullチェック
+		if (animalName == null ) {
+			// Nullが発生した場合に処理を止める
+			throw new IllegalArgumentException("引数にnullが含まれています");
+		}
+
+		// フィールド変数
+		this.animalName = animalName;
+		this.animalSize = animalSize;
+		this.animalSpeed = animalSpeed;
+	}
+
+	// 動物名のゲッター
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	// 動物名のセッター
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+
+	// 体長のセッター
+	public double getAnimalSize() {
+		return animalSize;
+	}
+
+	// 体長のゲッター
+	public void setAnimalSize(double animalSize) {
+		this.animalSize = animalSize;
+	}
+
+
+	// 速度のゲッター
+	public int getAnimalSpeed() {
+		return animalSpeed;
+	}
+
+	// 速度のセッター
+	public void setAnimalSpeed(int animalSpeed) {
+		this.animalSpeed = animalSpeed;
+	}
+
+}
